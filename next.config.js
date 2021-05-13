f40a48e9bbb84c82f021bba365de8f0f491d97ab
Jack LaFond <jack.lafond@icloud.com>
2021-05-12 22:14:58 -0400

    woops

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/invite',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  async redirects() {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/oauth2/authorize?client_id=840585628408217610&scope=bot&permissions=19456",
+        permanent: true,
+      },
+    ];
+  },
+};
