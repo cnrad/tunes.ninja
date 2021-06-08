eb30c2f733d2272a19c958f1fd775e275b89586a
Jack LaFond <6956351+jacc@users.noreply.github.com>
2021-06-07 23:21:28 -0400

    add Top.gg invite

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
           "https://discord.com/api/oauth2/authorize?client_id=840585628408217610&permissions=19456&scope=bot%20applications.commands",
         permanent: true,
       },
+      {
+        source: "/vote",
+        destination: "https://top.gg/bot/840585628408217610/vote",
+        permanent: true
+      }
     ];
   },
   target: "serverless",
